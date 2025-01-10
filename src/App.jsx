68d1a0af9310efce7b56da648bc1db1fdde16315
Jesse Kopala <jkopala@atrematech.com>
2025-01-10 14:17:41 -0700

    add and style components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import NewProject from "./components/NewProject";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      {/* Project Details Component */}
+      <NewProject />
     </main>
   );
 }
