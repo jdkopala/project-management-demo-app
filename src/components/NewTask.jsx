7c84fbe32025452e2e68013d98a7dddd47ecd976
Jesse Kopala <jdkopala@gmail.com>
2025-07-02 10:46:14 -0600

    Clear textbox when a task is created

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function NewTask(props) {
   const { onSaveTask } = props
   const [enteredTask, setEnteredTask] = useState('');
+
+  const inputRef = useRef();
 
   const handleChange = (event) => {
     setEnteredTask((event.target.value));
@@ -12,12 +14,13 @@ export default function NewTask(props) {
     if (enteredTask !== '') {
       onSaveTask(enteredTask);
       setEnteredTask('');
+      inputRef.current.value = '';
     }
   };
 
   return (
     <div className="flex flex-row items-center gap-4">
-      <input type="text" className="w-[16rem] px-2 py-1 rounded-sm border-b-2 border-stone-700 bg-stone-200" onChange={handleChange} />
+      <input ref={inputRef} type="text" className="w-[16rem] px-2 py-1 rounded-sm border-b-2 border-stone-700 bg-stone-200" onChange={handleChange} />
       <button className="text-stone-700 hover:text-stone-950" onClick={handleSaveTask}>+ Add Task</button>
     </div>
   )
